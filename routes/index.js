4491e941fde40f862845c4ed3fa3df9d4d19a26c
Nun-z <nickwitkin@gmail.com>
2021-06-02 02:09:17 -0700

    add project files

---
+++ routes/index.js
@@ -0,0 +1,20 @@
+var express = require('express');
+var router = express.Router();
+
+router.use(express.json()) // for parsing application/json
+router.use(express.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
+
+router.get('/', function(req, res)
+{
+  console.log("Starting!")
+});
+
+function getPipelineStatus(req, res, next) 
+{
+  //var pipelineName = req.query.pipelineName
+
+
+  next();// No need to return anything.
+}
+
+module.exports = router;
